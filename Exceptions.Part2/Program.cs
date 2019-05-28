8989af0622770f223ee667ad9e5436c930615a62
ascutariu <andreiscutariu92@gmail.com>
2019-05-28 21:03:10 +0300

    UPDATES

--- Exceptions.Part2/Program.cs
+++ Exceptions.Part2/Program.cs
@@ -1,14 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Exceptions.Part2
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var i = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    throw new WanstomeMathV2Exception();
+                }
+            }
+            catch (WanstomeMathV2Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (WantsomeBaseException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    public class WantsomeBaseException : ArithmeticException
+    {
+
+    }
+
+    [Serializable]
+    public class WanstomeMathV2Exception : WantsomeBaseException
+    {
+    }
+
+    [Serializable]
+    public class MyException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public MyException()
+        {
+        }
+
+        public MyException(string message) : base(message)
+        {
+        }
+
+        public MyException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected MyException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
         {
         }
     }
